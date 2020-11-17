4456e392c1c68717364514666334f27b0361cda7
DbencoPlanet <bernardamaeme@gmail.com>
2020-11-17 15:17:34 +0100

    HSPA User and Properties updated

---
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TabsetComponent } from 'ngx-bootstrap/tabs/public_api';
+import { IProperty } from '../IProperty.interface';
+
+@Component({
+  selector: 'app-add-property',
+  templateUrl: './add-property.component.html',
+  styleUrls: ['./add-property.component.css']
+})
+export class AddPropertyComponent implements OnInit {
+  @ViewChild('Form') addPropertyForm: NgForm;
+  @ViewChild('formTabs') formTabs: TabsetComponent;
+
+  propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex']
+
+  furnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished']
+
+  propertyView: IProperty = {
+    Id: null,
+    Name: '',
+    Price: null,
+    SellRent: null,
+    Type: null
+  };
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  onBack(){
+    this.router.navigate(['/'])
+  }
+
+  onSubmit(Form: NgForm){
+    console.log("Congrats Form submitted!");
+    console.log(this.addPropertyForm);
+  }
+
+  selectTab(tabId: number) {
+    this.formTabs.tabs[tabId].active = true;
+  }
+
+}
--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,18 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-card',
   //template: ``,
   templateUrl: 'property-card.component.html',
   //styles:['h1 {font-weight: normal;}']
-  styleUrls:['property-card.component.html']
+  styleUrls:['property-card.component.css']
 })
 export class PropertyCardComponent {
-  Property: any = {
-    "Id":1,
-    "Name":"Birla House",
-    "Type":"House",
-    "Price":12000
-  }
+  @Input() property : IProperty;
 
 }
